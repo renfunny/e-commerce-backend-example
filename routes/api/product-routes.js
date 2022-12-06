3b24df341e0ac0fd616e30c1f8e950745b30af75
Renato Cordova <ren99cordova@gmail.com>
2022-12-06 18:59:54 -0500

    finished product route, will continue to work on tag route and the rest

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -31,6 +31,34 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [`id`, `product_name`, `price`, `stock`],
+    include: [
+      {
+        model: Category,
+        attributes: [`category_name`],
+      },
+      {
+        model: Tag,
+        attributes: [`tag_name`],
+      },
+    ],
+  })
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({ message: `Product not found` });
+        return;
+      } else {
+        res.json(data);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
@@ -43,7 +71,13 @@ router.post("/", (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds,
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -109,6 +143,23 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({ message: `Product not found` });
+        return;
+      } else {
+        res.json(data);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
